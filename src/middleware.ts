53ab26fd5784715e3a564bbcca478504e5e44ab9
lucasroqe <alvim.lucas2@hotmail.com>
2025-04-08 20:23:23 -0300

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+ 
+export async function middleware(request: NextRequest) {
+	const sessionCookie = getSessionCookie(request);
+ 
+	if (!sessionCookie) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+ 
+	return NextResponse.next();
+}
+ 
+export const config = {
+	//Aplica o middleware em todas as rotas, exceto:
+	matcher: [
+	  '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|login|$).*)',
+	],
+  };
+  
