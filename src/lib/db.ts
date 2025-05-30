7bcb1f0e7e42e159e7fd717d3cfbed8ec35b52af
maucepinho <mauriciocepinho@gmail.com>
2025-05-30 00:03:51 -0300

    Retirar senha do banco no GitHub

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,5 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  host: 'br726.hostgator.com.br',
-  port: 3306,
-  user: 'hydros28',
-  password: 'A9302pbycW',
-  database: 'hydros28_estfrn02',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
 export async function query(sql: string, values?: any) {
   const connection = await pool.getConnection();
   try {
